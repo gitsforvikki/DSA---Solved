7643997632acccbad799f546284a5a6ea986112c
Vikash <vk6484412@gmail.com>
2026-01-18 12:18:22 +0530

    find second  largest number

--- 01-Basic/Basic.js
+++ 01-Basic/Basic.js
@@ -1,4 +1,4 @@
-let testArray = [12, -23, 0, -1, 145, 76, -90];
+let testArray = [12, -23, 0, -1, 145, 76, -90 , 190];
 
 /**
  *
@@ -28,4 +28,43 @@ function countNegativeElement(arr) {
   return count;
 }
 
-console.log(countNegativeElement(testArray));
+// console.log(countNegativeElement(testArray));
+
+/**
+ *
+ * write function to find out largest number in array
+ */
+
+function largestElement(arr) {
+  let largest = -Infinity;
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > largest) {
+      largest = arr[index];
+    }
+  }
+  return largest;
+}
+
+// console.log(largestElement(testArray));
+
+/**
+ * write function find second largest number in the array element
+ */
+function secondLargestNumber(arr) {
+  if (arr.length < 2) return "Not possible";
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > largest) {
+      secondLargest = largest;
+      largest = arr[index];
+    } else if (arr[index] < largest && arr[index] > secondLargest) {
+      secondLargest = arr[index];
+    }
+  }
+  if (secondLargest === -Infinity) return "Not Possible";
+  return secondLargest;
+}
+
+// console.log(secondLargestNumber(testArray));
